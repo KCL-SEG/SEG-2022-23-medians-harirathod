0daa1c3b890ffe90f9e82617f26851ccec8379c9
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-09-24 14:02:42 +0100

    Implement median function

--- median.py
+++ median.py
@@ -4,9 +4,19 @@
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
+        # List comprehension. Splits the input spring into an array of numbers, and converts each int to a float.
         numbers = [float(value) for value in input().split(",")]
+        # First we need to sort the list.
+        sorted_nums = sorted(numbers)
+        list_length = len(sorted_nums)
+        median_position = list_length // 2
+
+        if list_length % 2 == 1:
+            print(sorted_nums[median_position])
+        else:  
+            print(((sorted_nums[median_position] + sorted_nums[median_position - 1]) / 2))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+#print(numbers)
